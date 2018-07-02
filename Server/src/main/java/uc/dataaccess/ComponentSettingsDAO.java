f442946c12dadc4163e1a262e2cb01df7a64c6d2
Benjamin Basmaci <benjaminbasmaci@outlook.de>
2018-07-02 14:31:15 +0200

    bugfix

--- Server/src/main/java/uc/dataaccess/ComponentSettingsDAO.java
+++ Server/src/main/java/uc/dataaccess/ComponentSettingsDAO.java
@@ -9,64 +9,15 @@ import java.util.List;
 public class ComponentSettingsDAO extends DAOBase {
 
     public List<ComponentSettings> getComponentSettings(String piid) throws SQLException {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        List<ComponentSettings> result = new ArrayList<>();
-
-        try {
-            connection = getConnection();
-            statement = connection.prepareStatement("SELECT * FROM SIGNALS WHERE PI_ID = ? ORDER BY TIMESTAMP DESC LIMIT 60");
-            statement.setString(1, piid);
-            resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                ComponentSettings componentSetting = new ComponentSettings();
-                WaterFlowSensor waterFlowSensor = new WaterFlowSensor();
-                LightSensor lightSensor = new LightSensor();
-                WaterLevelSensor waterLevelSensor = new WaterLevelSensor();
-                Thermometer waterThermometer = new Thermometer();
-                Thermometer airThermometer = new Thermometer();
-                Lamp lamp = new Lamp();
-                Feeder feeder = new Feeder();
-                Pump pump = new Pump();
-
-                componentSetting.setPiId(resultSet.getString("PI_ID"));
-                componentSetting.setTimestamp(new java.util.Date(resultSet.getTimestamp("TIMESTAMP").getTime()));
-                waterFlowSensor.setValue(resultSet.getFloat("WATERFLOWSENSOR"));
-                componentSetting.setWaterFlowSensor(waterFlowSensor);
-                lightSensor.setValue(resultSet.getFloat("LIGHTSENSOR"));
-                componentSetting.setLightSensor(lightSensor);
-                waterLevelSensor.setValue(resultSet.getFloat("WATERLEVELSENSOR"));
-                componentSetting.setWaterLevelSensor(waterLevelSensor);
-                waterThermometer.setValue(resultSet.getFloat("WATER_THERMOMETER"));
-                componentSetting.setWaterThermometer(waterThermometer);
-                airThermometer.setValue(resultSet.getFloat("AIR_THERMOMETER"));
-                componentSetting.setAirThermometer(airThermometer);
-                lamp.setValue(resultSet.getFloat("LAMP"));
-                componentSetting.setLamp(lamp);
-                feeder.setValue(resultSet.getFloat("FEEDER"));
-                componentSetting.setFeeder(feeder);
-                pump.setValue(resultSet.getFloat("PUMP"));
-                componentSetting.setPump(pump);
-
-                result.add(componentSetting);
-            }
-
-        } finally {
-            if (resultSet != null)
-                resultSet.close();
-            if (statement != null)
-                statement.close();
-            if (connection != null)
-                connection.close();
-        }
-
-        return result;
+        getComponentSettingsBase(piid);
     }
 
-
     public List<ComponentSettings> getComponentSettings() throws SQLException {
+        getComponentSettingsBase(null);
+    }
+
+    private List<ComponentSettings> getComponentSettingsBase(String piid)
+    {
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
@@ -74,7 +25,12 @@ public class ComponentSettingsDAO extends DAOBase {
 
         try {
             connection = getConnection();
-            statement = connection.prepareStatement("SELECT * FROM SIGNALS ORDER BY TIMESTAMP DESC LIMIT 60");
+            if(piid == null || piid == "")
+                statement = connection.prepareStatement("SELECT * FROM SIGNALS ORDER BY TIMESTAMP DESC LIMIT 60");
+            else {
+                statement = connection.prepareStatement("SELECT * FROM SIGNALS WHERE PI_ID = ? ORDER BY TIMESTAMP DESC LIMIT 60");
+                statement.setString(1, piid);
+            }
             resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
